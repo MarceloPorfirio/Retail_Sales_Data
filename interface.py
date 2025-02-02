d5671e5acb50c465ce0fd6badb4609a95208dbb4
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2025-02-02 14:46:01 -0300

    add

--- interface.py
+++ interface.py
@@ -10,6 +10,14 @@ import base64  # Adicionado para codificar o buffer em base64
 # Carregar os dados
 df = pd.read_csv('retail_sales_cleaned_final.csv')
 
+
+df.rename(columns={'IsHoliday_x': 'Feriado'}, inplace=True)
+
+def calcular_impacto_feriados():
+    # Calcular a média de vendas em semanas com e sem feriado
+    impacto_feriados = df.groupby('Feriado')['Vendas_Semanais'].mean().reset_index()
+    return impacto_feriados
+
 # Função para calcular as top 10 lojas com maior e menor vendas
 def calcular_top_lojas():
     # Calcular vendas totais por loja
@@ -62,6 +70,21 @@ def criar_grafico_linha(dataframe, titulo_grafico):
     buf.seek(0)
     return buf
 
+def criar_grafico_barras_feriados(dataframe, titulo_grafico):
+    plt.figure(figsize=(8, 5))
+    sns.barplot(x='Feriado', y='Vendas_Semanais', data=dataframe, color='blue')
+    plt.title(titulo_grafico)
+    plt.xlabel('Feriado')
+    plt.ylabel('Média de Vendas Semanais')
+    plt.xticks(ticks=[0, 1], labels=['Sem Feriado', 'Com Feriado'])
+    
+    # Salvar o gráfico em um buffer
+    buf = BytesIO()
+    plt.savefig(buf, format='png', bbox_inches='tight')
+    plt.close()
+    buf.seek(0)
+    return buf
+
 # Função principal do Flet
 def main(page: ft.Page):
     page.title = "Análise de Vendas"
@@ -70,26 +93,11 @@ def main(page: ft.Page):
     page.padding = 20
 
     # Título da página
-    titulo = ft.Text("Análise de Vendas - Top 10 Lojas", size=24, weight="bold")
+    titulo = ft.Text("Análise de Vendas", size=24, weight="bold")
 
-    # Botões para exibir as análises
-    def exibir_top_10_maior(e):
-        top_10_maior, _ = calcular_top_lojas()
-        exibir_resultados(top_10_maior, "Top 10 Lojas com Maior Vendas", tipo_grafico='barras')
-
-    def exibir_top_10_menor(e):
-        _, top_10_menor = calcular_top_lojas()
-        exibir_resultados(top_10_menor, "Top 10 Lojas com Menor Vendas", tipo_grafico='barras')
-
-    def exibir_vendas_tempo(e):
-        vendas_por_data = calcular_vendas_por_data()
-        exibir_resultados(vendas_por_data, "Vendas ao Longo do Tempo", tipo_grafico='linha')
-
-    # Função para exibir os resultados em uma tabela e gráfico
     def exibir_resultados(dataframe, titulo_tabela, tipo_grafico='barras'):
-        # Limpar a página antes de exibir novos resultados
-        page.clean()
-        page.add(titulo)
+        # Limpar a área principal antes de exibir novos resultados
+        area_principal.controls.clear()
 
         # Criar uma tabela com os resultados (se aplicável)
         if tipo_grafico == 'barras':
@@ -107,6 +115,21 @@ def main(page: ft.Page):
                     ) for index, row in dataframe.iterrows()
                 ],
             )
+        elif tipo_grafico == 'barras_feriados':
+            tabela = ft.DataTable(
+                columns=[
+                    ft.DataColumn(ft.Text("Feriado")),
+                    ft.DataColumn(ft.Text("Média de Vendas Semanais")),
+                ],
+                rows=[
+                    ft.DataRow(
+                        cells=[
+                            ft.DataCell(ft.Text("Sem Feriado" if row['Feriado'] == 0 else "Com Feriado")),
+                            ft.DataCell(ft.Text(f"R$ {row['Vendas_Semanais']:,.2f}")),
+                        ]
+                    ) for index, row in dataframe.iterrows()
+                ],
+            )
         else:
             tabela = None
 
@@ -115,6 +138,8 @@ def main(page: ft.Page):
             grafico_buffer = criar_grafico_barras(dataframe, titulo_tabela)
         elif tipo_grafico == 'linha':
             grafico_buffer = criar_grafico_linha(dataframe, titulo_tabela)
+        elif tipo_grafico == 'barras_feriados':
+            grafico_buffer = criar_grafico_barras_feriados(dataframe, titulo_tabela)
 
         print("Gráfico gerado com sucesso! Tamanho do buffer:", len(grafico_buffer.getvalue()))  # Verificação
 
@@ -140,27 +165,100 @@ def main(page: ft.Page):
                 alignment=ft.MainAxisAlignment.CENTER,
             )
 
-        # Adicionar a linha à página
-        page.add(linha_resultados)
+        # Adicionar a linha à área principal
+        area_principal.controls.append(linha_resultados)
+        page.update()
 
-        # Botão para voltar ao menu
-        botao_voltar = ft.ElevatedButton("Voltar ao Menu", on_click=voltar_ao_menu)
-        page.add(botao_voltar)
+    def exibir_menu_principal(e):
+        ft.Text("Menu Principal")
 
-    # Função para voltar ao menu principal
-    def voltar_ao_menu(e):
-        page.clean()
-        page.add(titulo)
-        page.add(botao_maior_vendas, botao_menor_vendas, botao_vendas_tempo)
+    # Função para exibir as top 10 lojas com maior vendas
+    def exibir_top_10_maior(e):
+        top_10_maior, _ = calcular_top_lojas()
+        exibir_resultados(top_10_maior, "Top 10 Lojas com Maior Vendas", tipo_grafico='barras')
 
-    # Botões do menu principal
-    botao_maior_vendas = ft.ElevatedButton("Top 10 Lojas com Maior Vendas", on_click=exibir_top_10_maior)
-    botao_menor_vendas = ft.ElevatedButton("Top 10 Lojas com Menor Vendas", on_click=exibir_top_10_menor)
-    botao_vendas_tempo = ft.ElevatedButton("Vendas ao Longo do Tempo", on_click=exibir_vendas_tempo)
+    # Função para exibir as top 10 lojas com menor vendas
+    def exibir_top_10_menor(e):
+        _, top_10_menor = calcular_top_lojas()
+        exibir_resultados(top_10_menor, "Top 10 Lojas com Menor Vendas", tipo_grafico='barras')
 
-    # Adicionar elementos à página inicial
-    page.add(titulo)
-    page.add(botao_maior_vendas, botao_menor_vendas, botao_vendas_tempo)
+    # Função para exibir as vendas ao longo do tempo
+    def exibir_vendas_tempo(e):
+        vendas_por_data = calcular_vendas_por_data()
+        exibir_resultados(vendas_por_data, "Vendas ao Longo do Tempo", tipo_grafico='linha')
+
+    # Função para exibir o impacto de feriados
+    def exibir_impacto_feriados(e):
+        impacto_feriados = calcular_impacto_feriados()
+        exibir_resultados(impacto_feriados, "Impacto de Feriados nas Vendas", tipo_grafico='barras_feriados')
+
+    # Menu lateral
+    menu_lateral = ft.NavigationRail(
+        selected_index=0,
+        label_type=ft.NavigationRailLabelType.ALL,
+        min_width=100,
+        min_extended_width=200,
+        destinations=[
+            ft.NavigationRailDestination(
+                icon=ft.icons.HOME,
+                selected_icon=ft.icons.HOME_FILLED,
+                label="Inicio",
+            ),
+            
+            ft.NavigationRailDestination(
+                icon=ft.icons.TRENDING_UP,
+                selected_icon=ft.icons.TRENDING_UP_OUTLINED,
+                label="Top 10 Maiores Vendas",
+            ),
+            ft.NavigationRailDestination(
+                icon=ft.icons.TRENDING_DOWN,
+                selected_icon=ft.icons.TRENDING_DOWN_OUTLINED,
+                label="Top 10 Menores Vendas",
+            ),
+            ft.NavigationRailDestination(
+                icon=ft.icons.TIMELINE,
+                selected_icon=ft.icons.TIMELINE_OUTLINED,
+                label="Vendas ao Longo do Tempo",
+            ),
+            ft.NavigationRailDestination(
+                icon=ft.icons.HOLIDAY_VILLAGE,
+                selected_icon=ft.icons.HOLIDAY_VILLAGE_OUTLINED,
+                label="Impacto de Feriados",
+        ),
+        ],
+        on_change=lambda e: atualizar_tela(e.control.selected_index),
+    )
+
+    # Área principal
+    area_principal = ft.Column(alignment=ft.MainAxisAlignment.CENTER, expand=True)
+
+    # Função para atualizar a tela com base na seleção do menu
+    def atualizar_tela(index):
+        if index == 0:
+            exibir_menu_principal(None)
+        elif index == 1:
+            exibir_top_10_maior(None)
+        elif index == 2:
+            exibir_top_10_menor(None)
+        elif index == 3:
+            exibir_vendas_tempo(None)
+        elif index == 4:
+            exibir_impacto_feriados(None)
+
+    # Layout da página
+    page.add(
+        ft.Row(
+            [
+                menu_lateral,
+                ft.VerticalDivider(width=1),
+                area_principal,
+            ],
+            expand=True,
+        )
+    )
+
+    # Exibir a primeira análise ao carregar a página
+    atualizar_tela(0)
 
 # Executar o aplicativo Flet
 ft.app(target=main)
